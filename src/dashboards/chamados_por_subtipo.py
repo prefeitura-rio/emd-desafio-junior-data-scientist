f51acec4cca912aa4525ba2bf4cb96ce16e32b16
jessica <jessicasousa.pc@gmail.com>
2024-02-19 21:55:08 -0300

    :recycle:  refactor: incluir as funções de layout no dashboard de subtipo
    - Funções de layout criada em  #a9163ca

--- src/dashboards/chamados_por_subtipo.py
+++ src/dashboards/chamados_por_subtipo.py
@@ -1,12 +1,25 @@
 import datetime
 
+import pandas as pd
 import streamlit as st
 
 from src.plots import plot_bar_chart
 from src.plots import plot_calls_ts
+from src.utils import display_card
+from src.utils import display_dashboard_header
+from src.utils import make_grid
 
 
-def get_event(date, events):
+def get_event(date: datetime.date, events: pd.DataFrame) -> str | None:
+    """Obtém o evento associado a uma data, se houver.
+
+    Args:
+        date (datetime.date): A data para a qual se deseja obter o evento.
+        events (pd.DataFrame): O dataframe contendo os eventos e suas datas.
+
+    Returns:
+        str | None: O evento associado à data, se houver.
+    """
     event = events.loc[
         (date >= events["data_inicial"]) & (date <= events["data_final"]),
         "evento",
@@ -14,18 +27,45 @@ def get_event(date, events):
     return event.item() if event.shape[0] > 0 else None
 
 
-def get_subtypes(data):
+def get_subtypes(data: pd.DataFrame) -> list[str]:
+    """Obtém a lista de subtipos presentes nos dados.
+
+    Args:
+        data (pd.DataFrame): O dataframe contendo os chamados.
+
+    Returns:
+        List[str]: A lista de subtipos presentes nos dados.
+    """
     return data.subtipo.value_counts().index.tolist()
 
 
-def get_avg_calls(data):
+def get_avg_calls(data: pd.DataFrame) -> float:
+    """Calcula a média diária de chamados.
+
+    Args:
+        data (pd.DataFrame): O dataframe contendo os chamados.
+
+    Returns:
+        float: A média diária de chamados.
+    """
     if data.empty:
         return 0
     return data["data_inicio"].dt.date.value_counts().mean()
 
 
 @st.cache_data
-def get_calls_with_event(data, events):
+def get_calls_with_event(
+    data: pd.DataFrame, events: pd.DataFrame
+) -> pd.DataFrame:
+    """Filtra os chamados que ocorreram durante eventos.
+
+    Args:
+        data (pd.DataFrame): O dataframe contendo os chamados.
+        events (pd.DataFrame): O dataframe contendo os eventos.
+
+    Returns:
+        pd.DataFrame: O dataframe contendo os chamados que ocorreram durante eventos.
+    """
     calls_during_events = data.loc[
         data["data_inicio"].dt.date.between(
             events["data_inicial"].min(), events["data_final"].max()
@@ -36,24 +76,58 @@ def get_calls_with_event(data, events):
             lambda x: get_event(x, events)
         )
     )
-
     return calls_during_events
 
 
-def dashboard(calls, events):
-    st.markdown(
-        """
-        <header class="dashboard_header">
-            <h1 class="dashboard_title"> Análise de Chamados - <span class="highlighted">
-            Dashboard</span>
-            </h1>
-            <p class="dashboard_subtitle">Análise de chamados para um subtipo específico no período de 2022 a 2023</p>
-            <a href="https://www.1746.rio/hc/pt-br" target="_blank">
-                <img src="app/static/logo.jpeg" class="dashboard_logo">
-            </a>
-        </header>
-        """,
-        unsafe_allow_html=True,
+def display_cards(
+    calls_during_events: pd.DataFrame,
+    filtered_calls: pd.DataFrame,
+    subtype: str,
+) -> None:
+    """Exibe os cartões com informações sobre os chamados.
+
+    Args:
+        calls_during_events (pd.DataFrame): O dataframe contendo os chamados durante eventos.
+        filtered_calls (pd.DataFrame): O dataframe contendo os chamados filtrados por subtipo.
+        subtype (str): O subtipo de chamado selecionado.
+    """
+    grid = make_grid(rows=1, cols=5)
+    cards_data = [
+        ("Subtipo selecionado", subtype),
+        ("Chamados abertos desse subtipo", filtered_calls.shape[0]),
+        (
+            "Evento com mais chamados",
+            (
+                calls_during_events["durante_evento"].value_counts().idxmax()
+                if not calls_during_events.empty
+                else "Nenhum evento"
+            ),
+        ),
+        (
+            "Média diária durante todo o período",
+            f"{get_avg_calls(filtered_calls):.2f}",
+        ),
+        (
+            "Média diária durante eventos",
+            f"{get_avg_calls(calls_during_events.dropna(subset=['durante_evento'])):.2f}",
+        ),
+    ]
+
+    for i, (description, value) in enumerate(cards_data):
+        with grid[0][i]:
+            display_card(value, description)
+
+
+def dashboard(calls: pd.DataFrame, events: pd.DataFrame) -> None:
+    """Cria o dashboard para análise de chamados.
+
+    Args:
+        calls (pd.DataFrame): O dataframe contendo os chamados.
+        events (pd.DataFrame): O dataframe contendo os eventos.
+    """
+    display_dashboard_header(
+        "Análise de Chamados - Dashboard",
+        "Análise de chamados para um subtipo específico no período de 2022 a 2023",
     )
 
     min_date = datetime.date(2022, 1, 1)
@@ -100,85 +174,8 @@ def dashboard(calls, events):
     filtered_calls = calls[calls.subtipo == subtype]
     calls_during_events = get_calls_with_event(filtered_calls, events)
 
-    (
-        subtype_col,
-        calls_qty_cot,
-        event_max_avg_col,
-        avg_calls_col,
-        avg_calls_during_event_col,
-    ) = st.columns(5)
-
     with st.container():
-        with subtype_col:
-            st.markdown(
-                f"""
-                <div class="card">
-                    <h2 class="card_title"
-                    title="{subtype}"
-                    >{subtype}</h2>
-                    <p class="card_value">Subtipo selecionado</p>
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
-
-        with calls_qty_cot:
-            st.markdown(
-                f"""
-                <div class="card">
-                    <h2 class="card_title">{filtered_calls.shape[0]}</h2>
-                    <p class="card_value
-                    ">Chamados abertos desse subtipo</p>
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
-
-        with event_max_avg_col:
-            main_event = calls_during_events[
-                "durante_evento"
-            ].value_counts()  # .idxmax()
-            if main_event.empty:
-                main_event = "Nenhum evento"
-            else:
-                main_event = main_event.idxmax()
-            st.markdown(
-                f"""
-                <div class="card">
-                    <h2 class="card_title
-                    ">{main_event}</h2>
-                    <p class="card_value
-                    ">Evento com mais chamados</p>
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
-
-        with avg_calls_col:
-            st.markdown(
-                f"""
-                <div class="card">
-                    <h2 class="card_title
-                    ">{get_avg_calls(filtered_calls):.2f}</h2>
-                    <p class="card_value
-                    ">Média diária durante todo o período</p>
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
-
-        with avg_calls_during_event_col:
-            st.markdown(
-                f"""
-                <div class="card">
-                    <h2 class="card_title
-                    ">{get_avg_calls(calls_during_events.dropna(subset=["durante_evento"])):.2f}</h2>
-                    <p class="card_value
-                    ">Média diária durante eventos</p>
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
+        display_cards(calls_during_events, filtered_calls, subtype)
 
     with st.container():
         st.markdown(
