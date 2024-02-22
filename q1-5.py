65de6594e6c14a8a737596efd798d0d13ca95ccb
Milena Maia Araújo <milenamaiaaraujo@192.168.100.16>
2024-02-22 16:00:08 -0300

    answered question 3 on q1-5.py

--- q1-5.py
+++ q1-5.py
@@ -1,7 +1,3 @@
-
-# 1. Quantos chamados foram abertos no dia 01/04/2023? ✅
-# 2. Qual o tipo de chamado que teve mais teve chamados abertos no dia 01/04/2023?
-# 3. Quais os nomes dos 3 bairros que mais tiveram chamados abertos nesse dia?
 # 4. Qual o nome da subprefeitura com mais chamados abertos nesse dia?
 # 5. Existe algum chamado aberto nesse dia que não foi associado a um bairro ou subprefeitura na tabela de bairros? Se sim, por que isso acontece?
 
@@ -11,7 +7,7 @@ import basedosdados as bd
 
 main_path = "datario.administracao_servicos_publicos.chamado_1746"
 
-# Question 1
+# Questão 1 - Quantos chamados foram abertos no dia 01/04/2023? ✅
 quantity_query = f"SELECT COUNT(*) AS total_chamados_abertos FROM {main_path} WHERE DATE(data_inicio) = '2023-04-01';"
 
 df = bd.read_sql(quantity_query, billing_project_id= "dadosrio")
@@ -19,9 +15,30 @@ df = bd.read_sql(quantity_query, billing_project_id= "dadosrio")
 # print(f"\n\nO número total de chamados abertos em 2023-04-01 foi: {df}\n\n")
 
 
-# Question 2
+# Questão 2 - Qual o tipo de chamado que teve mais teve chamados abertos no dia 01/04/2023? ✅
 
 order_by_type_query = f"SELECT tipo, COUNT(*) AS total_ocorrencias FROM {main_path} GROUP BY tipo ORDER BY total_ocorrencias DESC LIMIT 1;"
 
 df = bd.read_sql(order_by_type_query, billing_project_id= "dadosrio")
-print(df)
+# print(type(df))
+# print(f"\n\n O tipo de chamado com maior número de ocorrências foi: {df}\n\n")
+
+# Questão 3 - Quais os nomes dos 3 bairros que mais tiveram chamados abertos nesse dia?
+
+location_query = f"""
+SELECT nome 
+FROM `datario.dados_mestres.bairro` 
+WHERE id_bairro = (
+    SELECT id_bairro
+    FROM (
+        SELECT id_bairro, COUNT(*) AS recorrencia_bairro 
+        FROM `datario.administracao_servicos_publicos.chamado_1746` 
+        GROUP BY id_bairro 
+        ORDER BY recorrencia_bairro DESC 
+        LIMIT 1
+    )
+);
+"""
+df = bd.read_sql(location_query, billing_project_id= "dadosrio")
+
+print(f"\n\n O nome do bairro com maior número de ocorrências foi: {df}\n\n")
